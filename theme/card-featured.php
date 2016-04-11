80dbed176de47143ba4d2e551995974c184920e9
RESL <moritz@resl.xyz>
2016-04-11 13:33:28 +0200

    info / featured

--- theme/card-featured.php
+++ theme/card-featured.php
@@ -1,8 +1,4 @@
 <article class="featured">
-   <!-- <img src="/wp-content/uploads/2016/03/ppp.png" style="max-height:500px; width:auto; max-width:100%;"><br /><br /> -->
-   <strong>Process — Studio for Art and Design</strong><br />
-   This is a stream of instagram images, tweets and finished projects.<br />
-
-   View finished &#8594; <a href="/works/">Works</a> only and more information about the &#8594; <a href="/about/">Studio</a>.
-   <!-- <img src="/wp-content/uploads/2015/11/preview_fb.png" style="max-height:500px; width:auto; max-width:100%;"> -->
+   <strong>Beniamin Urbanek</strong><br />
+   Animation & Film
 </article>
