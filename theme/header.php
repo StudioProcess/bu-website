f56bee02b8fc83659bcae2b45d8bc3886700b5d2
Martin Leopold <m@martinleopold.com>
2016-04-11 13:52:35 +0200

    removed pace.js

--- theme/header.php
+++ theme/header.php
@@ -12,8 +12,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 
-		<script src="http://www.process.studio/wp-content/themes/process2015/pace.min.js"></script>
-
 		<?php wp_head(); ?>
 
 	</head>
