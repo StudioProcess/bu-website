a899f3b72217729d734fc5ae492cadd50416f01f
RESL <moritz@resl.xyz>
2016-04-12 15:02:46 +0200

    layout

--- theme/card-featured.php
+++ theme/card-featured.php
@@ -1,4 +1,5 @@
 <article class="featured">
    <strong>Beniamin Urbanek</strong><br />
-   Works in Animation & Film.
+   is an animator and film maker based in Vienna, Austria.<br />
+   <a href="">mail@mail.at</a> / <a href="">Twitter</a> / <a href="">Instagram</a>
 </article>
